8c18a6c41608601edff2ed1fb866108830b256ee
Saptarsi Basu <basu.saptarsi91@gmail.com>
2023-02-04 15:15:54 +0530

    updates

--- build-scripts/pre_build.sh
+++ build-scripts/pre_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#CONFIG_FOLDER = "$CODEBUILD_SRC_DIR/script"
-#S3_CODECONFIG_FOLDER = "s3://s3-ftp-source/code"
-#echo "Copying code files..."
-#aws s3 cp $CONFIG_FOLDER/ $S3_CODECONFIG_FOLDER/ --recursive
+CONFIG_FOLDER = "$CODEBUILD_SRC_DIR/script"
+S3_CODECONFIG_FOLDER = "s3://s3-ftp-source/code"
+echo "Copying code files..."
+aws s3 cp $CONFIG_FOLDER/ $S3_CODECONFIG_FOLDER/ --recursive
 
 echo "Completed pre_build - $(date)"
